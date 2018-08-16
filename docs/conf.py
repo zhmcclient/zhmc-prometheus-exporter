15eb5e2048d8a563b9f28d09b74b2640ec16cecc
Jakob-Naucke <jakob.naucke@ibm.com>
2018-08-16 16:58:24 +0200

    Increased textwidth of documentation
    To further converge to the documentation of other zhmcclient projects,
    the textwidth is no longer limited.
    
    Signed-off-by: Jakob-Naucke <jakob.naucke@ibm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -81,7 +81,7 @@ html_theme = 'classic'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {"body_max_width": "100%"}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
